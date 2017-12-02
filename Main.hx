4b5fb9de7ee50be8bee8b896e94d0527ea911cc5
Aurel Bílý <aurel.bily@gmail.com>
2017-12-02 20:04:00 +0000

    tape reel

--- Main.hx
+++ Main.hx
@@ -16,6 +16,7 @@ typedef CFont = font.FontNS;
 
 class Main extends Application {
   public static var consoleFont:Font;
+  public static var am:AssetManager;
   public static var W:Int = 400;
   public static var H:Int = 300;
   
@@ -25,13 +26,62 @@ class Main extends Application {
         ,Optional(Window("", 800, 600))
         ,Surface(400, 300, 1)
         ,Assets([
-             CFont.embed()
+             Embed.getBitmap(CFont.ASSET_ID, "ns8x16.png")
             ,new AssetBind([CFont.ASSET_ID], (am, _) -> {
                 consoleFont = CFont.initAuto(am, 0xFFFFFFFF, null, 0xFF999999);
                 false;
               })
             ,Embed.getBitmap("city", "png/city.png")
             ,Embed.getBitmap("interface", "png/interface.png")
+            
+            /*
+            ,Embed.getSound("Action1", "wav/Action1.wav")
+            ,Embed.getSound("Action2", "wav/IgnoranceIsBliss/Music/Action2.wav")
+            ,Embed.getSound("ActionChaos", "wav/IgnoranceIsBliss/Music/ActionChaos.wav")
+            ,Embed.getSound("ActionChaos2", "wav/IgnoranceIsBliss/Music/ActionChaos2.wav")
+            ,Embed.getSound("ComfySong", "wav/IgnoranceIsBliss/Music/ComfySong.wav")
+            ,Embed.getSound("DroneChill", "wav/IgnoranceIsBliss/Music/DroneChill.wav")
+            ,Embed.getSound("EasterEgg", "wav/IgnoranceIsBliss/Music/EasterEgg.wav")
+            ,Embed.getSound("EndingRampantClueless", "wav/IgnoranceIsBliss/Music/EndingRampantClueless.wav")
+            ,Embed.getSound("EndingRampantMarch", "wav/IgnoranceIsBliss/Music/EndingRampantMarch.wav")
+            ,Embed.getSound("EndingRampantRebellion", "wav/IgnoranceIsBliss/Music/EndingRampantRebellion.wav")
+            ,Embed.getSound("EndingRampantRebellionNoVoice", "wav/IgnoranceIsBliss/Music/EndingRampantRebellionNoVoice.wav")
+            ,Embed.getSound("EndingSad", "wav/IgnoranceIsBliss/Music/EndingSad.wav")
+            ,Embed.getSound("EndingTriumph", "wav/IgnoranceIsBliss/Music/EndingTriumph.wav")
+            ,Embed.getSound("HackerConversation", "wav/IgnoranceIsBliss/Music/HackerConversation.wav")
+            ,Embed.getSound("IntenseExo", "wav/IgnoranceIsBliss/Music/IntenseExo.wav")
+            ,Embed.getSound("JingelLong", "wav/IgnoranceIsBliss/Music/JingelLong.wav")
+            ,Embed.getSound("JingelRingShort", "wav/IgnoranceIsBliss/Music/JingelRingShort.wav")
+            ,Embed.getSound("NotComfySong", "wav/IgnoranceIsBliss/Music/NotComfySong.wav")
+            ,Embed.getSound("NotSoComfySong", "wav/IgnoranceIsBliss/Music/NotSoComfySong.wav")
+            ,Embed.getSound("Unused1", "wav/IgnoranceIsBliss/Music/Unused1.wav")
+            ,Embed.getSound("Unused2", "wav/IgnoranceIsBliss/Music/Unused2.wav")
+            */
+            ,Embed.getSound("Alarm", "wav/IgnoranceIsBliss/SFX/Alarm.wav")
+            ,Embed.getSound("BootUp", "wav/IgnoranceIsBliss/SFX/BootUp.wav")
+            ,Embed.getSound("BootUp2", "wav/IgnoranceIsBliss/SFX/BootUp2.wav")
+            ,Embed.getSound("CasettePlay", "wav/IgnoranceIsBliss/SFX/CasettePlay.wav")
+            ,Embed.getSound("CasetteStop", "wav/IgnoranceIsBliss/SFX/CasetteStop.wav")
+            ,Embed.getSound("Click", "wav/IgnoranceIsBliss/SFX/Click.wav")
+            ,Embed.getSound("Click2", "wav/IgnoranceIsBliss/SFX/Click2.wav")
+            ,Embed.getSound("ClickError", "wav/IgnoranceIsBliss/SFX/ClickError.wav")
+            ,Embed.getSound("MenuClick", "wav/IgnoranceIsBliss/SFX/MenuClick.wav")
+            ,Embed.getSound("MenuThump", "wav/IgnoranceIsBliss/SFX/MenuThump.wav")
+            ,Embed.getSound("RingtoneShort", "wav/IgnoranceIsBliss/SFX/RingtoneShort.wav")
+            ,Embed.getSound("Scan", "wav/IgnoranceIsBliss/SFX/Scan.wav")
+            ,Embed.getSound("SelectVideoGamey", "wav/IgnoranceIsBliss/SFX/SelectVideoGamey.wav")
+            ,Embed.getSound("Shutdown1", "wav/IgnoranceIsBliss/SFX/Shutdown1.wav")
+            ,Embed.getSound("Shutdown2", "wav/IgnoranceIsBliss/SFX/Shutdown2.wav")
+            ,Embed.getSound("ShutdownBig", "wav/IgnoranceIsBliss/SFX/ShutdownBig.wav")
+            ,Embed.getSound("TurnOffPCDisplay", "wav/IgnoranceIsBliss/SFX/TurnOffPCDisplay.wav")
+            ,Embed.getSound("Typewriter1", "wav/IgnoranceIsBliss/SFX/Typewriter1.wav")
+            ,Embed.getSound("Typewriter2", "wav/IgnoranceIsBliss/SFX/Typewriter2.wav")
+            ,Embed.getSound("Typewriter3", "wav/IgnoranceIsBliss/SFX/Typewriter3.wav")
+            ,Embed.getSound("Typewriter4", "wav/IgnoranceIsBliss/SFX/Typewriter4.wav")
+            ,Embed.getSound("Typewriter5", "wav/IgnoranceIsBliss/SFX/Typewriter5.wav")
+            ,Embed.getSound("ZoomIn", "wav/IgnoranceIsBliss/SFX/ZoomIn.wav")
+            ,Embed.getSound("ZoomOut", "wav/IgnoranceIsBliss/SFX/ZoomOut.wav")
+            
             ,Pal.bind()
             ,UI.bind()
           ])
@@ -40,6 +90,7 @@ class Main extends Application {
         ,Console
         //,ConsoleRemote("localhost", 8001)
       ]);
+    am = assetManager;
     preloader = new DummyPreloader(this, "dummy");
     addState(new Dummy(this));
     #if flash
@@ -57,6 +108,7 @@ class Dummy extends JamState {
   var ui:UI;
   
   override public function to() {
+    //amS("Action2").play(Forever);
     c = City.make(amB("city"));
     ren = new CityRen(400, 300);
     ui = new UI();
@@ -66,5 +118,13 @@ class Dummy extends JamState {
     ab.fill(Pal.colours[0]);
     ren.render(ab, c);
     ui.render(ab);
+  }
+  
+  override public function mouseDown(mx:Int, my:Int):Void {
+    ui.mouseDown(mx, my);
+  }
+  
+  override public function mouseUp(mx:Int, my:Int):Void {
+    ui.mouseUp(mx, my);
   }
 }
--- lib/UI.hx
+++ lib/UI.hx
@@ -1,10 +1,13 @@
 package lib;
 
-import sk.thenet.FM;
+import haxe.ds.Vector;
 import sk.thenet.anim.*;
 import sk.thenet.app.asset.Bind as AssetBind;
 import sk.thenet.bmp.*;
 import sk.thenet.bmp.manip.*;
+import sk.thenet.plat.Platform;
+
+using sk.thenet.FM;
 
 class UI {
   public static function bind():AssetBind {
@@ -12,21 +15,65 @@ class UI {
         var f = am.getBitmap("interface").fluent;
         b_crt = f >> new Cut(0, 0, 123, 102);
         b_tapeControl = f >> new Cut(0, 102, 137, 26);
-        b_tapeControlDown = [ for (i in 0...5) f >> new Cut(1 + 27 * i, 128, 27, 26) ];
+        b_tapeControlDown
+          = Vector.fromArrayCopy([ for (i in 0...5) f >> new Cut(1 + 27 * i, 128, 27, 26) ]);
         b_transcript = f >> new Cut(0, 154, 137, 71);
+        b_transcriptWheel
+          = Vector.fromArrayCopy([ for (i in 0...3) f >> new Cut(i * 5, 225, 5, 17) ]
+          .concat([ for (i in 0...3) f >> new Cut(16 + i * 4, 225, 4, 16) ]));
+        b_cursor
+          = Vector.fromArrayCopy([ for (i in 0...4) f >> new Cut(128 + i * 16, 16, 16, 16) ]);
+        b_tape = new Vector<Bitmap>(6);
+        b_tape[0] = f >> new Cut(0, 242, 158, 158);
+        trace(Pal.colours[21].toStringHex());
+        b_tape[1] = b_tape[0].fluent
+          >> new ReplaceColour(Pal.colours[20], Pal.colours[11])
+          << new ReplaceColour(Pal.colours[17], Pal.colours[13])
+          << new ReplaceColour(Pal.colours[21], Pal.colours[12])
+          << new ReplaceColour(Pal.colours[22], Pal.colours[12]);
+        b_tape[2] = b_tape[1].fluent
+          >> new ReplaceColour(Pal.colours[11], Pal.colours[1])
+          << new ReplaceColour(Pal.colours[13], Pal.colours[1])
+          << new ReplaceColour(Pal.colours[12], Pal.colours[9]);
+        b_tape[3] = f >> new Cut(0, 400, 158, 158);
+        b_tape[4] = b_tape[3].fluent >> new ReplaceColour(Pal.colours[1], 0);
+        b_tape[3].fluent << new ReplaceColour(Pal.colours[20], 0);
+        b_tapeOpening = Vector.fromArrayCopy([
+            for (i in 0...60) b_tape[3].fluent >> new Rotate((i / 60) * Math.PI * 2)
+          ]);
+        b_tapeMerged = Vector.fromArrayCopy([
+            for (i in 0...60) {
+              var ret = Platform.createBitmap(b_tape[0].width * 2, b_tape[0].height * 2, 0).fluent;
+              ret << new Blit(b_tape[1], 79, 79)
+                << new Blit(b_tape[2].fluent >> new Copy() << new AlphaMask(b_tape[4], true), 79, 79)
+                << new Blit(b_tape[2].fluent >> new Grow(79, 79, 79, 79) << new AlphaMask(b_tapeOpening[i], true), -2, -1)
+                << new Blit(b_tape[0].fluent >> new Grow(79, 79, 79, 79) << new AlphaMask(b_tapeOpening[i], true), 2, 1)
+                << new Blit(b_tape[2].fluent >> new Grow(79, 79, 79, 79) << new AlphaMask(b_tapeOpening[i], true));
+              ret;
+            }
+          ]);
         false;
       });
   }
   
   static var b_crt:Bitmap;
   static var b_tapeControl:Bitmap;
-  static var b_tapeControlDown:Array<Bitmap>;
+  static var b_tapeControlDown:Vector<Bitmap>;
   static var b_transcript:Bitmap;
+  static var b_transcriptWheel:Vector<Bitmap>;
+  static var b_cursor:Vector<Bitmap>;
+  static var b_tape:Vector<Bitmap>;
+  static var b_tapeOpening:Vector<Bitmap>;
+  static var b_tapeMerged:Vector<Bitmap>;
   
+  public var held:HeldButton = None;
   public var crt:Bitween;
   public var tapeControl:Bitween;
   public var transcript:Bitween;
-  public var tcDown:Int;
+  public var trWheel:Int;
+  public var hover:Int;
+  public var tapePh:Float;
+  public var tapeSpeed:Float;
   
   public function new() {
     crt = new Bitween(60);
@@ -35,21 +82,79 @@ class UI {
     crt.setTo(true);
     tapeControl.setTo(true);
     transcript.setTo(true);
-    tcDown = -1;
+    trWheel = 0;
+    hover = 0;
+    tapePh = 0;
+    tapeSpeed = 0;
   }
   
   public function render(to:Bitmap):Void {
-    if (FM.prng.nextMod(10) == 0) tcDown = FM.prng.nextMod(6) - 1;
     to.blitAlpha(b_crt, 0, Main.H - Timing.quintOut.getI(crt.valueF, 99));
     var tcY = Main.H - Timing.quintOut.getI(tapeControl.valueF, 92);
     to.blitAlpha(b_tapeControl, 123, tcY);
-    if (tcDown != -1) {
-      to.blitAlpha(b_tapeControlDown[tcDown], 124 + tcDown * 27, tcY);
+    switch (held) {
+      case TapeControl(i):
+      to.blitAlpha(b_tapeControlDown[i], 124 + i * 27, tcY);
+      case _:
     }
     var trY = Main.H - Timing.quintOut.getI(transcript.valueF, 71);
     to.blitAlpha(b_transcript, 123, trY);
+    to.blitAlpha(b_transcriptWheel[2 - (trWheel >> 2)], 123, trY);
+    to.blitAlpha(b_transcriptWheel[5 - (trWheel >> 2)], 256, trY);
+    trWheel++;
+    trWheel %= 12;
     crt.tick();
     tapeControl.tick();
     transcript.tick();
+    /*
+    to.blitAlpha(b_tape[0], 2, 2);
+    to.blitAlpha(b_tape[1], 22, 22);
+    to.blitAlpha(b_tape[2], 42, 42);
+    to.blitAlpha(b_tape[3], 62, 62);
+    to.blitAlpha(b_tape[4], 82, 82);
+    */
+    to.blitAlpha(b_tapeMerged[tapePh.floor().clampI(0, 59)], 263 - 79, 133 - 79);
+    
+    tapeSpeed = Platform.mouse.x / 100.0;
+    tapePh += tapeSpeed;
+    if (tapePh < 0) tapePh += 60;
+    if (tapePh >= 60) tapePh -= 60;
+    
+    var mx = Platform.mouse.x;
+    var my = Platform.mouse.y;
+    #if flash
+    if (mx.withinI(0, Main.W - 1) && my.withinI(0, Main.H - 1)) {
+      flash.ui.Mouse.hide();
+    }
+    #end
+    to.blitAlpha(b_cursor[1 + (hover >> 3)], mx, my);
+    hover++;
+    hover %= 24;
   }
+  
+  public function mouseDown(mx:Int, my:Int):Void {
+    held = None;
+    for (i in 0...5) {
+      if (mx.withinI(124 + i * 27, 124 + i * 27 + 26)
+          && my.withinI(Main.H - 92, Main.H - 92 + 21)) {
+        Main.am.getSound("CasettePlay").play();
+        held = TapeControl(i);
+        break;
+      }
+    }
+  }
+  
+  public function mouseUp(mx:Int, my:Int):Void {
+    switch (held) {
+      case TapeControl(_):
+      Main.am.getSound("CasetteStop").play();
+      case _:
+    }
+    held = None;
+  }
+}
+
+enum HeldButton {
+  None;
+  TapeControl(i:Int);
 }
